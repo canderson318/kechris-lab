226730fb43de659bd67ecdc3be04dccae59a03fb
canderson138 <christianjbanderson@gmail.com>
2026-01-15 12:44:50 -0700

    fixing setup script; cloning RCFGL repo

--- smoking-networks/analysis-versions/version001/analysis/002-condition-specific-networks.py
+++ smoking-networks/analysis-versions/version001/analysis/002-condition-specific-networks.py
@@ -1,2 +1,16 @@
+#!/Users/canderson/miniconda3/envs/smoknet-env/bin/python
 
-import numpy as np
+import pandas as pd
+import sys
+import os
+from sklearn.preprocessing import StandardScaler
+
+RCFGL_path = '/Users/canderson/Documents/school/local-kechris-lab/kechris-lab/smoking-networks/RCFGL'
+os.chdir(RCFGL_path)
+sys.path.insert(0, 'Python_functions')
+from RCFGL import RFGL, RCFGL
+
+os.chdir(RCFGL_path)
+from Dstream_functions import*
+
+
---
+++ smoking-networks/setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+### Create Environment ###
+conda env create -f environment.yaml
+
+### Activate Environment ###
+# workaround `conda init`
+source "$(conda info --base)/etc/profile.d/conda.sh" 
+conda activate smoknet-env
+
+### Install other dependencies ###
+python -m pip install cppyy
+python -m pip install pycparser cffi # used for prox_tv build
+# pip install  --no-build-isolation prox_tv
+pip install prox_tv
+
+### Install RFGL repo ###
+git clone https://github.com/sealx017/RCFGL.git
